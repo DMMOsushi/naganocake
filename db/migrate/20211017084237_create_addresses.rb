ae2e689ed619faad3fbb0694b57375cf1f7d4ee2
n.you.01222@gmail.com <ec2-user@ip-172-31-94-249.ec2.internal>
2021-10-17 08:49:28 +0000

    model add

---
+++ db/migrate/20211017084237_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      
+      t.integer :customer_id
+      t.string  :destination
+      t.string  :address
+      t.string  :postalcode
+
+      t.timestamps
+    end
+  end
+end
