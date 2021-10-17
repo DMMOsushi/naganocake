2adab4f1e24d7d7053fd9bbebdd70d4f89de9c64
n.you.01222@gmail.com <ec2-user@ip-172-31-94-249.ec2.internal>
2021-10-17 09:28:11 +0000

    コントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,79 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/create'
+    get 'orders/thanks'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/create'
+    get 'cart_items/destroy_all'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/update'
+    get 'items/destroy'
+    get 'items/create'
+    get 'items/destroy_all'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/update'
+    get 'addresses/create'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'orders_items/update'
+  end
+  namespace :admin do
+    get 'orders/show'
+    get 'orders/update'
+  end
+  namespace :admin do
+    get 'categories/create'
+    get 'categories/index'
+    get 'categories/edit'
+    get 'categories/update'
+  end
+  namespace :admin do
+    get 'items/new'
+    get 'items/create'
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
   devise_for :public,skip: [:passwords,], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
