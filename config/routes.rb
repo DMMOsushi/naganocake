e89e690a3f6064c9bb543a53438e0895b028da03
Sakamura-akifumi <tmr20th08moon@gmail.com>
2021-10-17 07:17:43 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :publics,skip: [:passwords,], controllers: {
+    registrations: "publics/registrations",
+    sessions: 'publics/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
