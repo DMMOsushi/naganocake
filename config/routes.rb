4ff6ceffb167b1fce78a47ae8a3aa9ff91550ea7
masaa0802 <masaakigoto0802@gmail.com>
2021-10-19 06:24:29 +0000

    ユーザー情報機能

--- config/routes.rb
+++ config/routes.rb
@@ -47,7 +47,6 @@ Rails.application.routes.draw do
 
     resources :order_items, only: [:update]
      
-    get 'top2'
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
