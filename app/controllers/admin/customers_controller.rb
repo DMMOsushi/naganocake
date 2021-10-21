25ec1af0fab359125eb9c2f4ea7e9bea976ed839
KouOsada <kouosada.0930@gmail.com>
2021-10-21 03:41:49 +0000

    [Add]show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,28 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @publics = Public.all
   end
 
   def show
+    @public = Public.find(params[:id])
   end
 
   def edit
+    @public = Public.find(params[:id])
   end
 
   def update
+    @public = Public.find(params[:id])
+    if @public.update(public_params)
+      redirect_to admin_customer_path(@public)
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def public_params
+    params.require(:public).permit(:first_name, :last_name, :first_kana_name, :last_kana_name, :address, :postalcode, :telephone, :email, :sale_status)
   end
 end
