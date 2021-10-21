3633275ae4f042db42ba4ec918460465f3d76770
Sakamura-akifumi <tmr20th08moon@gmail.com>
2021-10-21 03:17:39 +0000

    配送先機能index/edit

--- app/models/public.rb
+++ app/models/public.rb
@@ -3,4 +3,7 @@ class Public < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :addresses, dependent: :destroy
+
 end
