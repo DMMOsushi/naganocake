ff0ce71930850a7658bb6cdaaf4eeb2c72234005
numayuu <n.you.01222@gmail.com>
2021-10-22 07:51:54 +0000

    add

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,8 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @public = current_public
+    @sum = 0
+    @items = Item.all
   end
 
   def update
@@ -9,8 +12,27 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = current_public.cart_items.new(cart_item_params)
+    @cart_item.public_id = current_public.id
+    if current_public.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_public.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_quantity = @cart_item.cart_quantity + cart_item.cart_quantity
+      cart_item.update(cart_quantity: @cart_quantity)
+      redirect_to public_params_cart_items_url(@cart_item.public)
+    elsif
+      @cart_item.save
+      redirect_to public_cart_items_url(@cart_item.public)
+    else
+      redirect_to items_url
+    end
   end
 
   def destroy_all
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :public_id, :cart_quantity)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,15 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_public!, except: [:index, :show]
+
   def index
+    @public = current_public
+    @categories = Category.all
+    @items = Item.all
+    @total_items = @sell_items
   end
 
   def show
+    @item = Item.find(params[:id])
+    @categories = Category.all
   end
 end
