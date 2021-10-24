193d14713c71b80d61f968a21d8f87423eeadcf7
KouOsada <kouosada.0930@gmail.com>
2021-10-24 07:52:11 +0000

    [Add]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,18 +1,34 @@
 class Admin::OrdersController < ApplicationController
 
-  before_action :authenticate_admin!
+   before_action :authenticate_admin!
+
+  def index
+    if params[:day]
+      @orders = Order.created_today
+    else
+  	  @orders = Order.all
+    end
+  end
 
   def show
-    @admin = current
+    @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: @order)
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash[:success] = "注文ステータスを更新しました"
+      redirect_to admin_orders_path
+    else
+      render 'show'
+    end
   end
 
   private
 
   def order_params
-    params.require(:order).params(:name)
+    params.require(:order).permit(:order_status)
   end
 
 end
